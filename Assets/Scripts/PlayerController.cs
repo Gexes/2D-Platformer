33dbe30ff1b1b89454cd26f4b391765af16c1428
Gexes <145797289+Gexes@users.noreply.github.com>
2023-11-14 13:50:00 -0500

    11/14
    - added animations
    - added new parameters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,66 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; //importing SceneManagement library
 
 public class PlayerController : MonoBehaviour
 {
-    //Movement variable
-    Rigidbody2D rb;//creates reference for rigibody beause jump requires physics
-    public float jumpForce;// the force that will be added to the vertical component of player's velocity
-    public float speed = 0.5f;
+    //Animation Variables
+    Animator anim;
+    public bool moving = false;
+    //Movement Variables
+    Rigidbody2D rb; //create reference for rigidbody bc jump requires physics
+    public float jumpForce; //the force that will be added to the vertical component of player's velocity
+    public float speed;
 
 
-    public static PlayerController instance; //creating an object of the class to be findable
-
-    //Ground Check variables
+    //Ground Check Variables
     public LayerMask groundLayer;
     public Transform groundCheck;
     public bool isGrounded;
+
+    SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        {
-
-        }
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded =
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, .5f, groundLayer);
+
         Vector3 newPosition = transform.position;
         Vector3 newScale = transform.localScale;
         float currentScale = Mathf.Abs(transform.localScale.x);
 
-        if (Input.GetKey("s"))
-        {
-            //player moves down
-            newPosition.y -= speed;
-        }
-
-        //player moves left
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
         {
             newPosition.x -= speed;
             newScale.x = -currentScale;
+            moving = true;
         }
 
-        //player moves right
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition.x += speed;
             newScale.x = currentScale;
+            moving = true;
         }
 
-        transform.position = newPosition;
-        transform.localScale = newScale;
-
-        if (Input.GetKey("w") || Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded == true)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        if ((Input.GetKeyDown("a")) || Input.GetKeyUp("d") || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            moving = false;
+        }
+
+
+        transform.position = newPosition;
+        transform.localScale = newScale;
     }
 }
