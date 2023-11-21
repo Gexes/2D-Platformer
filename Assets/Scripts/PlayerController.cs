50348e94da7e81b94b5968242db18103876bf93c
Gexes <145797289+Gexes@users.noreply.github.com>
2023-11-20 20:13:06 -0500

    11/20
    - Added more sections
    - Added Start/End Screen
    - Added Animation
    - Added Music
    - Added start sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -64,5 +65,14 @@ public class PlayerController : MonoBehaviour
 
         transform.position = newPosition;
         transform.localScale = newScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("exit"))
+        {
+            SceneManager.LoadScene(2);
+        }
+
     }
 }
